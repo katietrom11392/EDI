445f61d9e3bf1e52491af62e3e32c9237f1387ad
PythonPlusPlus-PPP <73278436+PythonPlusPlus-PPP@users.noreply.github.com>
2020-11-28 23:08:41 -0800

    WTH is going on with my commits?

--- EDI/databaseconnection.cpp
+++ EDI/databaseconnection.cpp
@@ -12,10 +12,8 @@ DatabaseConnection::DatabaseConnection(QWidget *parent) : QMainWindow(parent)
 QSqlDatabase DatabaseConnection::establishConnection(QString connectionName){
     db = QSqlDatabase::addDatabase("QMYSQL", connectionName);
     db.setHostName("");
-//    db.setUserName("ediCEO370");
-//    db.setPassword("miguelCEO370");
-    db.setUserName("root");
-    db.setPassword("CtrlAltDlt55%%");
+    db.setUserName("ediCEO370");
+    db.setPassword("miguelCEO370");
     db.setDatabaseName("EDI");
     return db;
 }
